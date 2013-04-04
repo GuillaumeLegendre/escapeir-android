706b85a08193dcb539e49f90e21ef9a6e22e75f0
Marc Saragosa <marc.saragosa@gmail.com>
2013-04-04 17:48:44 +0200

    test display image

--- EscapeIR-Android/gen/fr/umlv/escape/R.java
+++ EscapeIR-Android/gen/fr/umlv/escape/R.java
@@ -21,8 +21,9 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int level1=0x7f020001;
+        public static final int default_ship_player=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int level1=0x7f020002;
     }
     public static final class id {
         public static final int LoadingProgress=0x7f080000;
--- EscapeIR-Android/src/fr/umlv/escape/MainActivity.java
+++ EscapeIR-Android/src/fr/umlv/escape/MainActivity.java
@@ -1,8 +1,6 @@
 package fr.umlv.escape;
 
 import java.util.concurrent.atomic.AtomicBoolean;
-
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -12,6 +10,7 @@ import android.widget.ProgressBar;
 
 public class MainActivity extends Activity {
 	ProgressBar loadingBar;
+	
 	Handler handler = new Handler() {
 		@Override
 		public void handleMessage(Message msg){
