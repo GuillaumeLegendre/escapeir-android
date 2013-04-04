b2f37cb3ea0e337e33551473ad0f6a709676bca5
Marc Saragosa <marc.saragosa@gmail.com>
2013-04-04 20:14:15 +0200

    first try for the new front

--- EscapeIR-Android/src/fr/umlv/escape/front/Drawable.java
+++ EscapeIR-Android/src/fr/umlv/escape/front/Drawable.java
@@ -1,5 +1,8 @@
 package fr.umlv.escape.front;
 
+import android.graphics.Bitmap;
+
+
 public interface Drawable {
 	/**
 	 * Says if the {@link Drawable} should be drawn or not.
@@ -11,4 +14,19 @@ public interface Drawable {
 	 * Specify that the {@link Drawable} should not be drawn anymore.
 	 */
 	public void doNotDrawAnymore(); // trouver un meilleur nom ^^
+	
+	/**
+	 * @return The {@link Bitmap} to draw.
+	 */
+	public Bitmap getImage();
+	
+	/**
+	 * @return The center position X of the {@link Drawable}.
+	 */
+	public float getPosX();
+
+	/**
+	 * @return The center position Y of the {@link Drawable}.
+	 */
+	public float getPosY();
 }
