f181e2687dc5c36dac3985a046160955810bea67
Marc Saragosa <marc.saragosa@gmail.com>
2013-04-18 11:26:12 +0200

    fix

--- EscapeIR-Android/gen/fr/umlv/escape/R.java
+++ EscapeIR-Android/gen/fr/umlv/escape/R.java
@@ -22,17 +22,26 @@ public final class R {
     }
     public static final class drawable {
         public static final int default_ship_player=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int level=0x7f020002;
-        public static final int level2=0x7f020003;
-        public static final int level3=0x7f020004;
+        public static final int editor_button=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int launcher=0x7f020003;
+        public static final int launcher_land=0x7f020004;
+        public static final int level1=0x7f020005;
+        public static final int level2=0x7f020006;
+        public static final int level3=0x7f020007;
+        public static final int main_menu=0x7f020008;
+        public static final int main_menu_land=0x7f020009;
+        public static final int play_button=0x7f02000a;
     }
     public static final class id {
+        public static final int EditorButton=0x7f080002;
         public static final int LoadingProgress=0x7f080000;
-        public static final int action_settings=0x7f080001;
+        public static final int PlayButton=0x7f080001;
+        public static final int action_settings=0x7f080003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int main_menu=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -40,7 +49,9 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int editorButton=0x7f050003;
+        public static final int loading=0x7f050004;
+        public static final int playButton=0x7f050002;
     }
     public static final class style {
         /** 
