fc4f3851c789667e9fa59de09ba35496f229d616
Guillaume <guillaume.legendre@boosket.com>
2013-05-26 16:11:04 +0200

    spinner select

--- EscapeIR-Android/src/fr/umlv/escape/editor/EditorMapActivity.java
+++ EscapeIR-Android/src/fr/umlv/escape/editor/EditorMapActivity.java
@@ -6,16 +6,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
-public class EditorMapActivity extends Activity{
+public class EditorMapActivity extends Activity implements OnItemSelectedListener{
+	Spinner spinner_map;
+	String map = "level1";
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.editor_map);
 		
-		Spinner spinner = (Spinner) findViewById(R.id.wave_list_select);
+		spinner_map = (Spinner) findViewById(R.id.wave_list_select);
 		
 		// Create an ArrayAdapter using the string array and a default spinner layout
 		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
@@ -23,8 +29,9 @@ public class EditorMapActivity extends Activity{
 		// Specify the layout to use when the list of choices appears
 		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		// Apply the adapter to the spinner
-		spinner.setAdapter(adapter);
-
+		spinner_map.setAdapter(adapter);
+		
+		spinner_map.setOnItemSelectedListener(this);
 	}
 	
 	@Override
@@ -39,11 +46,23 @@ public class EditorMapActivity extends Activity{
 		switch (item.getItemId()) {
 		case R.id.action_save:
 			Intent intent = new Intent(this, EditorLevelActivity.class);
-			intent.putExtra("map", "test");
+			intent.putExtra("map", map);
 			startActivity(intent);
 			return true;
 		default:
 			return super.onOptionsItemSelected(item);
 		}	
+	}
+
+	@Override
+	public void onItemSelected(AdapterView<?> parent, View view, int pos,
+			long id) {
+		map = (String) parent.getItemAtPosition(pos);
+	}
+
+	@Override
+	public void onNothingSelected(AdapterView<?> parent) {
+		// TODO Auto-generated method stub
+		
 	}
 }
