a9bd9b61da6c832e6971c4d3b03aabb4adfa8447
Guillaume <guillaume.legendre@boosket.com>
2013-05-26 15:19:44 +0200

    add map

---
+++ EscapeIR-Android/src/fr/umlv/escape/editor/EditorMapActivity.java
@@ -0,0 +1,48 @@
+package fr.umlv.escape.editor;
+
+import fr.umlv.escape.R;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+
+public class EditorMapActivity extends Activity{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.editor_map);
+		
+		
+		
+//		// Create an ArrayAdapter using the string array and a default spinner layout
+//		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+//		        R.array.planets_array, android.R.layout.simple_spinner_item);
+//		// Specify the layout to use when the list of choices appears
+//		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//		// Apply the adapter to the spinner
+//		spinner.setAdapter(adapter);
+
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.save, menu);
+		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.action_save:
+			Intent intent = new Intent(this, EditorLevelActivity.class);
+			intent.putExtra("map", "test");
+			startActivity(intent);
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}	
+	}
+}
